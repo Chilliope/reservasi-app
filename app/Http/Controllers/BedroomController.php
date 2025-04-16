31d4e440d830d4dacb9f80664e95666e376fb9b1
Erlang Andriyanputra <chilliope48@gmail.com>
2025-04-16 10:55:45 +0800

    update bedroom

--- app/Http/Controllers/BedroomController.php
+++ app/Http/Controllers/BedroomController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Bedroom;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class BedroomController extends Controller
@@ -30,7 +31,7 @@ class BedroomController extends Controller
     {
         $validated = Validator::make($request->all(), [
             'type_id' => 'required',
-            'image' => 'required'
+            'image' => 'required|mimes:png,jpg'
         ]);
 
         if($validated->fails()) {
@@ -50,6 +51,39 @@ class BedroomController extends Controller
             'type_id' => $request->type_id,
             'image' => $newFileName
         ]);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $bedroom
+        ], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = Validator::make($request->all(), [
+            'type_id' => 'required',
+            'image' => 'required|mimes:png,jpg'
+        ]);
+
+        if($validated->fails()) {
+            return response()->json($validated->errors(), 400);
+        }
+
+        $bedroom = Bedroom::where('id', $id)->first();
+
+        if($request->file('image')) {
+            Storage::delete($bedroom->image);
+            $file = $request->file('image');
+            $fileExt = $file->getClientOriginalExtension();
+            $random = md5(uniqid(mt_rand(), true));
+
+            $newFileName = $random . '.' . $fileExt;
+            $file->move('storage/bedroom', $newFileName);
+            $bedroom->image = $newFileName;
+        }   
+
+        $bedroom->type_id = $request->type_id;
+        $bedroom->save();
 
         return response()->json([
             'status' => 'success',
